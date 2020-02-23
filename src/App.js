867364c0ddc7c048866947ea2f5ec771c4a25a0d
Tina Hang <mstinahang@gmail.com>
2020-02-23 19:11:13 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
+				<MouseComponent />
 			</div>
 		);
 	}
